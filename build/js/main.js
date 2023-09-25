66c6f9b050546016451fbbe6a17ee817e76ee95f
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-09-25 14:28:58 +0600

    as assertion

--- build/js/main.js
+++ build/js/main.js
@@ -1,67 +1,31 @@
 "use strict";
-//? type alias
-//? literal types
-let myName;
-let userName;
-userName = "jhon";
-//? function
-const sum = (a, b) => {
-    return a + b;
+let a = "hello";
+let b = a; // less specific
+let c = a; // more specific
+let d = "pi";
+let e = "pi";
+const addOrConcat = (a, b, c) => {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
 };
-const logMsg = (message) => {
-    console.log(message);
-};
-logMsg("hi");
-logMsg(sum(1, 2));
-logMsg(sum(5, 1));
-let subtract = function (c, d) {
-    return c - d;
-};
-// interface mathFunction {
-//   (a: number, b: number): number;
+let myVal = addOrConcat(12, 14, "add");
+let nextVal = addOrConcat(12, 14, "add");
+// 10 as string;
+10;
+const img = document.querySelector("img");
+const myImg = document.getElementById("#img");
+// img.src;
+// myImg.src;
+// let year: HTMLElement | null;
+// year = document.getElementById("year");
+// let thisYear: string;
+// thisYear = new Date().getFullYear().toString();
+// if (year) {
+//   year.setAttribute("datetime", thisYear);
+//   year.textContent = thisYear;
 // }
-let multiply = function (c, d) {
-    return c * d;
-};
-logMsg(multiply(213, 32));
-//? optional parameter
-const addAll = (a, b, c) => {
-    if (typeof c !== "undefined") {
-        return a + b + c;
-    }
-    return a + b;
-};
-const sumAll = (a = 10, b, c = 2) => {
-    return a + b + c;
-};
-logMsg(addAll(2, 3, 5));
-logMsg(sumAll(4, 1));
-logMsg(sumAll(undefined, 2));
-// ? rest parameters
-const total = (a, ...nums) => {
-    return a + nums.reduce((prev, curr) => prev + curr);
-};
-logMsg(total(12, 13, 14, 15));
-const createError = (errMsg) => {
-    throw new Error(errMsg);
-};
-const infinite = () => {
-    let i = 1;
-    while (true) {
-        i++;
-        if (i > 100)
-            break;
-    }
-};
-// custom type guard
-const isNumber = (value) => {
-    return typeof value === "number" ? true : false;
-};
-// use of never typee
-const numberOrString = (value) => {
-    if (isNumber(value))
-        return "string";
-    if (isNumber(value))
-        return "number";
-    return createError("This should never happen!");
-};
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
--- src/main.ts
+++ src/main.ts
@@ -1,95 +1,46 @@
-//? type alias
+type One = string;
+type Two = string | number;
+type Three = "hello";
 
-type stringOrNumber = string | number;
-type stringOrNumberArray = (string | number)[];
+let a: One = "hello";
+let b = a as Two; // less specific
+let c = a as Three; // more specific
 
-type gt = {
-  name?: string;
-  active: boolean;
-  album: stringOrNumberArray;
+let d = <One>"pi";
+let e = <string | number>"pi";
+
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: "add" | "concat"
+): number | string => {
+  if (c === "add") return a + b;
+  return "" + a + b;
 };
 
-type userId = stringOrNumber;
+let myVal: string = addOrConcat(12, 14, "add") as string;
+let nextVal: number = addOrConcat(12, 14, "add") as number;
 
-//? literal types
-let myName: "pieash";
+// 10 as string;
+10 as unknown as string;
 
-let userName: "any" | "jhon" | "Kartus";
+const img = document.querySelector("img")!;
+const myImg = document.getElementById("#img") as HTMLImageElement;
 
-userName = "jhon";
+// img.src;
+// myImg.src;
 
-//? function
+// let year: HTMLElement | null;
+// year = document.getElementById("year");
 
-const sum = (a: number, b: number): number => {
-  return a + b;
-};
-
-const logMsg = (message: any): void => {
-  console.log(message);
-};
-
-logMsg("hi");
-logMsg(sum(1, 2));
-logMsg(sum(5, 1));
-
-let subtract = function (c: number, d: number): number {
-  return c - d;
-};
-
-type mathFunction = (a: number, b: number) => number;
-// interface mathFunction {
-//   (a: number, b: number): number;
+// let thisYear: string;
+// thisYear = new Date().getFullYear().toString();
+// if (year) {
+//   year.setAttribute("datetime", thisYear);
+//   year.textContent = thisYear;
 // }
 
-let multiply: mathFunction = function (c, d) {
-  return c * d;
-};
-
-logMsg(multiply(213, 32));
-
-//? optional parameter
-
-const addAll = (a: number, b: number, c?: number) => {
-  if (typeof c !== "undefined") {
-    return a + b + c;
-  }
-  return a + b;
-};
-const sumAll = (a: number = 10, b: number, c: number = 2) => {
-  return a + b + c;
-};
-
-logMsg(addAll(2, 3, 5));
-logMsg(sumAll(4, 1));
-logMsg(sumAll(undefined, 2));
-
-// ? rest parameters
-const total = (a: number, ...nums: number[]): number => {
-  return a + nums.reduce((prev, curr) => prev + curr);
-};
-
-logMsg(total(12, 13, 14, 15));
-
-const createError = (errMsg: string): never => {
-  throw new Error(errMsg);
-};
-
-const infinite = () => {
-  let i: number = 1;
-  while (true) {
-    i++;
-    if (i > 100) break;
-  }
-};
-
-// custom type guard
-const isNumber = (value: any): boolean => {
-  return typeof value === "number" ? true : false;
-};
-
-// use of never typee
-const numberOrString = (value: number | string): string => {
-  if (isNumber(value)) return "string";
-  if (isNumber(value)) return "number";
-  return createError("This should never happen!");
-};
+const year = document.getElementById("year") as HTMLSpanElement;
+const thisYear: string = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
